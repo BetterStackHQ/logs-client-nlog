43ea3784b847da21fa6117f36df40498bb8e306e
Petr Heinz <petr@betterstack.com>
2023-11-06 12:34:34 +0100

    improve comments in Program.cs

--- example-project/Program.cs
+++ example-project/Program.cs
@@ -13,27 +13,27 @@ NLog.Config.ConfigurationItemFactory.Default.ValueFormatter = new BetterStack.Lo
 // Create logger for current class
 var logger = LogManager.GetCurrentClassLogger();
 
-//Following code showcases 6 NLog's default log levels
-//Additionaly, it also show how to structure logs to add additional data
+// Following code showcases 6 NLog's default log levels
+// Additionaly, it also show how to structure logs to add additional data
 
-//Ttrace the code using the Trace() method
+// Trace the code using the Trace() method
 logger.Trace("Tracing the code!");
 
-//Send debug messages using the Debug() method
+// Send debug messages using the Debug() method
 logger.Debug("Debugging is hard, but can be easier with Better Stack!");
 
-//Send informative messages about application progress using the Info() method
-//All of the properties that you pass to the log will be stored in a structured
-//form in the context section of the logged event
+// Send informative messages about application progress using the Info() method
+// All of the properties that you pass to the log will be stored in a structured
+// form in the context section of the logged event
 logger.Info("User {user} - {userID} just ordered item {item}", "Josh", 95845, 75423);
 
-//Report non-critical issues using the Warn() method
+// Report non-critical issues using the Warn() method
 logger.Warn("Something is happening!");
 
-//Send message about serious problems using the Error() method
+// Send message about serious problems using the Error() method
 logger.Error("Error occurred! And it's not good.");
 
-//Report fatal errors that coused application to crash using the Fatal() method
+// Report fatal errors that caused application to crash using the Fatal() method
 logger.Fatal("Application crashed! Needs to be fixed ASAP!");
 
 Console.WriteLine("All done! Now, you can check Better Stack to see your logs");
